c2094df51e96eaf92e018055cb66b5a5bd3f2096
Ian Emerik Leko <leko.ian.emerik@gmail.com>
2023-06-27 07:40:01 +0100

    removed db files

--- db.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_pymongo import pymongo
-from app import app
-
-CONNECTION_STRING = "mongodb+srv://<username>:<password>@flask-mongodb-atlas-1g8po.mongodb.net/test?retryWrites=true&w=majority"
-client = pymongo.MongoClient(CONNECTION_STRING)
-db = client.get_database('<your_db_name>')
-user_collection = pymongo.collection.Collection(db, '<your_collection_name>')
