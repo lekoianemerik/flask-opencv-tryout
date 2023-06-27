075614bc2f38b3119c7fd7112fe57538fa9375b8
Ian Emerik Leko <leko.ian.emerik@gmail.com>
2023-06-27 07:16:15 +0100

    fully working server, alpha version prob. cv2 tries to get 5 biggest contours of objects in the photo

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+import os, shutil
 from flask import Flask, render_template, request, session
 from utils import *
 app = Flask(__name__)
@@ -14,42 +14,50 @@ os.path.dirname("../templates")
 
 @app.route('/')
 def main():
-    return render_template("index.html")
+    print('HERE1')
+    if request.method == 'GET':
+        print('HERE2')
+        print(UPLOAD_FOLDER)
+        print(os.listdir(UPLOAD_FOLDER))
+        for filename in os.listdir(UPLOAD_FOLDER):
+            filepath = (os.path.join(UPLOAD_FOLDER, filename))
+            try:
+                os.remove(filepath)
+                print(f'deleted {filepath}')
+            except Exception as e:
+                print('Failed to delete %s. Reason: %s' % (filepath, e))
+        for filename in os.listdir(OUTPUT_FOLDER):
+            filepath = (os.path.join(OUTPUT_FOLDER, filename))
+            try:
+                os.remove(filepath)
+                print(f'deleted {filepath}')
+            except Exception as e:
+                print('Failed to delete %s. Reason: %s' % (filepath, e))
+
+    return render_template('index.html')
 
 @app.route('/', methods=["POST"])
 def uploadFile():
-
+    
     if request.method == 'POST':
         _img = request.files['file-uploaded']
         filename = _img.filename
-        #allowed_file(filename)
         _img.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         session['uploaded_img_file_path'] = os.path.join(app.config['UPLOAD_FOLDER'], filename)
 
         return render_template('index.html', success=True)
+
     
-@app.route('/show_file')
+
+    
+@app.route('/show_file', methods=['GET', 'POST'])
 def displayImage():
 
     img_file_path = session.get('uploaded_img_file_path', None)
-    if img_file_path.split(".")[-1] in ("mp4", "mov"):
-        return render_template('show_file.html', user_image=img_file_path, is_image = False, is_show_button=True)
-    else:
-        return render_template('show_file.html', user_image = img_file_path, is_image= True, is_show_button=True)
-    
+    output_image_path, response, file_type = blur_it_up(img_file_path)
 
-@app.route('/detect_object')
-def detectObject():
-
-    uploaded_image_path = session.get('uploaded_img_file_path', None)
-    output_image_path, response, file_type = blur_it_up(uploaded_image_path)
-
-    file_type = 'image'
-    if file_type == "image":
-        return render_template('show_file.html',  user_image=output_image_path, is_image= True, is_show_button=False)
-    else:
-        return render_template('show_file.html',  user_image= output_image_path, is_image= False, is_show_button=False)
-
+    print(output_image_path)
+    return render_template('show_file.html', image_uploaded=True, original_image=img_file_path, transformed_image=output_image_path)    
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -14,13 +14,30 @@ def blur_it_up(img_path):
     
     filename = img_path.split("/")[-1].split(".")[0]
     output_image_path = os.path.join(
-        OUTPUT_FOLDER,  # Specified path using the image filename
-        'output_image_{name}.jpg'.format(name=filename)
+        OUTPUT_FOLDER,
+        f'output_image_{filename}.jpg'
     )
     print(f"========================\nImage processed: {output_image_path}\n")  # Print current image's filename
 
-    output = cv2.blur(image, (17, 17))
+    image = imutils.resize(image, height=800)
+    image = imutils.rotate_bound(image, 0)
 
-    cv2.imwrite(output_image_path, output)
+    channel_names = ['Blue', 'Green', 'Red']
+    channels = cv2.split(image)
+    masks = []
+    for i, channel in enumerate(channels):
+        channel = cv2.blur(channel, (11, 11))
+        channel = cv2.Canny(channel, 20, 50)
+        masks.append(channel)
+
+    mask = cv2.bitwise_or(masks[0], cv2.bitwise_or(masks[1], masks[2]))
+
+    cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
+
+    cv2.drawContours(image, cnts, -1, (0, 255, 255), 2)
+
+    cv2.imwrite(output_image_path, image)
 
     return output_image_path, None, None
